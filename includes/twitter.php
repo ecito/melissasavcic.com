ef9024f0767dda4a152b7f96983a406f9ba4ae1f
Andre Navarro <andre.navarro@gmail.com>
2010-01-31 19:52:26 -0600

    fixed twitter

--- includes/twitter.php
+++ includes/twitter.php
@@ -1,1 +1,33 @@
-<div id="juitterContainer"></div><img src="images/sidebar-bot.gif"></img>
+<div class="twitter_container">
+<?php 
+// require the twitter library
+require "twitter.lib.php";
+
+// your twitter username and password
+$username = "MelissaSavcic";
+$password = "mel2ssa";
+
+// initialize the twitter class
+$twitter = new Twitter($username, $password);
+
+// fetch public timeline in xml format
+$options = array('count' => 2);
+$xml = $twitter->getUserTimeline($options);
+
+$twitter_status = new SimpleXMLElement($xml);
+foreach($twitter_status->status as $status){
+foreach($status->user as $user){
+echo '<img src="'.$user->profile_image_url.'" class="twitter_image">';
+echo '<a href="http://www.twitter.com/MelissaSavcic">'.$user->name.'</a> ';
+}
+echo $status->text;
+echo '<br/>';
+echo '<br/>';
+//echo '<div class="twitter_posted_at">Posted at:'.$status->created_at.'</div>';
+}
+echo '</div>';
+
+?>
+
+
+<img src="images/sidebar-bot.gif"></img>
